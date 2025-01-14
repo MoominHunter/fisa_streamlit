446b8f65c37f3754632b45b4e080f8e874b0ea8d
MoominHunter <osaen1842@naver.com>
2025-01-14 17:13:13 +0900

    feat: streamlit test

--- pages/search_bar.py
+++ pages/search_bar.py
@@ -10,9 +10,11 @@ img_list = ['https://i.imgur.com/t2ewhfH.png',
             'https://i.imgur.com/ECROFMC.png', 
             'https://i.imgur.com/MDKQoDc.jpg']
 
-search = st.text_input("검색하실 애니메이션을 입력하세요")
+ani = st.text_input(""검색하실 애니메이션을 입력하세요"")
 
-
-for ani in ani_list:
-    if search in ani:
-        img_dix = ani_list.index(search)
+if ani == ""짱구는못말려"":
+    st.image('https://i.imgur.com/t2ewhfH.png')
+elif ani == ""몬스터"":
+    st.image('https://i.imgur.com/ECROFMC.png')
+elif ani == ""릭앤모티"":
+    st.image('https://i.imgur.com/MDKQoDc.jpg')
